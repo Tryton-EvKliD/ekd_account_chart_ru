5b76b17c3272597640049cb816ba0bd6f9b784d5
Dmitry Klimanov <k-dmitry2@narod.ru>
2011-03-31 12:44:56 +0700

    second commit

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-/home/trytond-1.8.2/trytond/modules/ekd_acc_chart_ru/__init__.py
+# -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
--- __tryton__.py
+++ __tryton__.py
@@ -1,1 +1,25 @@
-/home/trytond-1.8.2/trytond/modules/ekd_acc_chart_ru/__tryton__.py
+# -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Charts of Accounts Russian Standart',
+    'name_ru_RU': 'План счетов Российский стандарт',
+    'version': '1.8.0',
+    'author': 'Dmitry Klimanov',
+    'email': 'k-dmitry2@narod.ru',
+    'website': 'http://www.delfi2000.ru/',
+    'description': '''Chart of Account Russian Standart
+''',
+    'description_ru_RU': '''План счетов Российский стандарт
+''',
+    'depends': [
+        'ekd_account',
+    ],
+    'xml': [
+         'xml/account_type_ru.xml',
+         'xml/account_chart_ru.xml',
+    ],
+    'translation': [
+#        'fr_FR.csv',
+    ],
+}
